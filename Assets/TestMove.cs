5b08a5576df1205e326d088b3cf1b6871b728231
AndrewLorets <andrewlorets@gmail.com>
2024-04-05 19:47:45 +0500

    Update

---
+++ Assets/TestMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour
+{
+    //[SerializeField] private int _speed = 5; 
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Translate(transform.forward * _speed * Time.deltaTime); 
+    //}
+}
